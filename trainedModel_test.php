a59b0ceea8084b1286c3e0c66f512f63b2152173
1zTuzhu <shawnz.utas@gmail.com>
2025-05-11 20:02:50 +1000

    trained model test php update

---
+++ trainedModel_test.php
@@ -0,0 +1,21 @@
+<?php
+error_reporting(E_ALL);			//show any errors if there if any
+ini_set('display_errors', '1');
+header('Content-type: text/plain');
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Phpml\ModelManager;
+$modelManager = new ModelManager();
+
+$mlp = $modelManager -> restoreFromFile(__DIR__ . "/mlp_trained_model.dat");
+
+$temp = isset($_GET['temp']) ? floatval($_GET['temp']) : null;
+
+if ($temp === null) {
+    echo "No temperature value provided.";
+    exit;
+}
+
+$normalized = [$temp / 100];
+
+echo $mlp->predict($normalized);
